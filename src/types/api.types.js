0cce81fdbed042d30fe5bf5510c2f2ff1822e8c8
Yefribrioso3 <yefribrioso3@gmail.com>
2022-07-15 11:20:36 -0400

    carga masiva

--- src/types/api.types.js
+++ src/types/api.types.js
@@ -1,12 +1,13 @@
-const localApi = "http://localHost:3001/api";
+// const localApi = "http://localHost:3001/api";
 
 // const herokuApi = "https://node-gead.herokuapp.com/api";
 
-// const abiApi = "http://172.18.220.69:4001/api";
+const abiApi = "172.18.220.69:3001/api";
+// const abiApi = "http://172.18.220.69:3001/api";
 
-const ApiProductivo = "https://mazappsupply.ab-inbev.com/gead-back/api" 
+// const ApiProductivo = "https://mazappsupply.ab-inbev.com/gead-back/api" 
 
-export const globalApi = ApiProductivo;
+export const globalApi = abiApi;
 
 // webpack-dev-server
 
